174d47850257ce0a1ff8a485f8f2222ed33585a6
Brenno Luan <brennoluan8530@gmail.com>
2026-01-16 20:47:34 -0300

    feat: add product cards for gaming monitor in main layout

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,253 @@
         </template>
       </v-app-bar>
 
-      <v-main>
-        <HelloWorld />
+      <v-main class="mt-6">
+        <v-container>
+
+          <v-row>
+            <v-col cols="12" sm="6" md="4" lg="3">
+              <v-card flat class="border">
+                <div class="px-6 py-2">
+                  <v-img
+                    src="https://images.kabum.com.br/produtos/fotos/659038/monitor-gamer-aoc-24-fhd-ips-180hz-0-5ms-hdmi-e-dp-w-led-antireflexo-ajuste-de-altura-preto-24g4-p_1733834296_m.jpg"></v-img>
+                </div>
+                <v-card-text>
+                  <h4>Monitor Gamer AOC 24", Full HD, 180Hz, 0.5ms, IPS, DisplayPort e HDMI, Altura Ajustável, Preto -
+                    24G4/P
+                  </h4>
+                  <div class="my-4">
+                    <div class="font-weight-light text-decoration-line-through mb-n2">R$ 1.477,26</div>
+                    <div class="text-h5 font-weight-bold">R$ 749,99</div>
+                    <div class="font-weight-light">À vista no PIX</div>
+                  </div>
+                  <v-btn flat block color="primary">Adicionar</v-btn>
+                </v-card-text>
+              </v-card>
+            </v-col>
+
+            <v-col cols="12" sm="6" md="4" lg="3">
+              <v-card flat class="border">
+                <div class="px-6 py-2">
+                  <v-img
+                    src="https://images.kabum.com.br/produtos/fotos/659038/monitor-gamer-aoc-24-fhd-ips-180hz-0-5ms-hdmi-e-dp-w-led-antireflexo-ajuste-de-altura-preto-24g4-p_1733834296_m.jpg"></v-img>
+                </div>
+                <v-card-text>
+                  <h4>Monitor Gamer AOC 24", Full HD, 180Hz, 0.5ms, IPS, DisplayPort e HDMI, Altura Ajustável, Preto -
+                    24G4/P
+                  </h4>
+                  <div class="my-4">
+                    <div class="font-weight-light text-decoration-line-through mb-n2">R$ 1.477,26</div>
+                    <div class="text-h5 font-weight-bold">R$ 749,99</div>
+                    <div class="font-weight-light">À vista no PIX</div>
+                  </div>
+                  <v-btn flat block color="primary">Adicionar</v-btn>
+                </v-card-text>
+              </v-card>
+            </v-col>
+
+            <v-col cols="12" sm="6" md="4" lg="3">
+              <v-card flat class="border">
+                <div class="px-6 py-2">
+                  <v-img
+                    src="https://images.kabum.com.br/produtos/fotos/659038/monitor-gamer-aoc-24-fhd-ips-180hz-0-5ms-hdmi-e-dp-w-led-antireflexo-ajuste-de-altura-preto-24g4-p_1733834296_m.jpg"></v-img>
+                </div>
+                <v-card-text>
+                  <h4>Monitor Gamer AOC 24", Full HD, 180Hz, 0.5ms, IPS, DisplayPort e HDMI, Altura Ajustável, Preto -
+                    24G4/P
+                  </h4>
+                  <div class="my-4">
+                    <div class="font-weight-light text-decoration-line-through mb-n2">R$ 1.477,26</div>
+                    <div class="text-h5 font-weight-bold">R$ 749,99</div>
+                    <div class="font-weight-light">À vista no PIX</div>
+                  </div>
+                  <v-btn flat block color="primary">Adicionar</v-btn>
+                </v-card-text>
+              </v-card>
+            </v-col>
+
+            <v-col cols="12" sm="6" md="4" lg="3">
+              <v-card flat class="border">
+                <div class="px-6 py-2">
+                  <v-img
+                    src="https://images.kabum.com.br/produtos/fotos/659038/monitor-gamer-aoc-24-fhd-ips-180hz-0-5ms-hdmi-e-dp-w-led-antireflexo-ajuste-de-altura-preto-24g4-p_1733834296_m.jpg"></v-img>
+                </div>
+                <v-card-text>
+                  <h4>Monitor Gamer AOC 24", Full HD, 180Hz, 0.5ms, IPS, DisplayPort e HDMI, Altura Ajustável, Preto -
+                    24G4/P
+                  </h4>
+                  <div class="my-4">
+                    <div class="font-weight-light text-decoration-line-through mb-n2">R$ 1.477,26</div>
+                    <div class="text-h5 font-weight-bold">R$ 749,99</div>
+                    <div class="font-weight-light">À vista no PIX</div>
+                  </div>
+                  <v-btn flat block color="primary">Adicionar</v-btn>
+                </v-card-text>
+              </v-card>
+            </v-col>
+
+            <v-col cols="12" sm="6" md="4" lg="3">
+              <v-card flat class="border">
+                <div class="px-6 py-2">
+                  <v-img
+                    src="https://images.kabum.com.br/produtos/fotos/659038/monitor-gamer-aoc-24-fhd-ips-180hz-0-5ms-hdmi-e-dp-w-led-antireflexo-ajuste-de-altura-preto-24g4-p_1733834296_m.jpg"></v-img>
+                </div>
+                <v-card-text>
+                  <h4>Monitor Gamer AOC 24", Full HD, 180Hz, 0.5ms, IPS, DisplayPort e HDMI, Altura Ajustável, Preto -
+                    24G4/P
+                  </h4>
+                  <div class="my-4">
+                    <div class="font-weight-light text-decoration-line-through mb-n2">R$ 1.477,26</div>
+                    <div class="text-h5 font-weight-bold">R$ 749,99</div>
+                    <div class="font-weight-light">À vista no PIX</div>
+                  </div>
+                  <v-btn flat block color="primary">Adicionar</v-btn>
+                </v-card-text>
+              </v-card>
+            </v-col>
+
+            <v-col cols="12" sm="6" md="4" lg="3">
+              <v-card flat class="border">
+                <div class="px-6 py-2">
+                  <v-img
+                    src="https://images.kabum.com.br/produtos/fotos/659038/monitor-gamer-aoc-24-fhd-ips-180hz-0-5ms-hdmi-e-dp-w-led-antireflexo-ajuste-de-altura-preto-24g4-p_1733834296_m.jpg"></v-img>
+                </div>
+                <v-card-text>
+                  <h4>Monitor Gamer AOC 24", Full HD, 180Hz, 0.5ms, IPS, DisplayPort e HDMI, Altura Ajustável, Preto -
+                    24G4/P
+                  </h4>
+                  <div class="my-4">
+                    <div class="font-weight-light text-decoration-line-through mb-n2">R$ 1.477,26</div>
+                    <div class="text-h5 font-weight-bold">R$ 749,99</div>
+                    <div class="font-weight-light">À vista no PIX</div>
+                  </div>
+                  <v-btn flat block color="primary">Adicionar</v-btn>
+                </v-card-text>
+              </v-card>
+            </v-col>
+
+            <v-col cols="12" sm="6" md="4" lg="3">
+              <v-card flat class="border">
+                <div class="px-6 py-2">
+                  <v-img
+                    src="https://images.kabum.com.br/produtos/fotos/659038/monitor-gamer-aoc-24-fhd-ips-180hz-0-5ms-hdmi-e-dp-w-led-antireflexo-ajuste-de-altura-preto-24g4-p_1733834296_m.jpg"></v-img>
+                </div>
+                <v-card-text>
+                  <h4>Monitor Gamer AOC 24", Full HD, 180Hz, 0.5ms, IPS, DisplayPort e HDMI, Altura Ajustável, Preto -
+                    24G4/P
+                  </h4>
+                  <div class="my-4">
+                    <div class="font-weight-light text-decoration-line-through mb-n2">R$ 1.477,26</div>
+                    <div class="text-h5 font-weight-bold">R$ 749,99</div>
+                    <div class="font-weight-light">À vista no PIX</div>
+                  </div>
+                  <v-btn flat block color="primary">Adicionar</v-btn>
+                </v-card-text>
+              </v-card>
+            </v-col>
+
+            <v-col cols="12" sm="6" md="4" lg="3">
+              <v-card flat class="border">
+                <div class="px-6 py-2">
+                  <v-img
+                    src="https://images.kabum.com.br/produtos/fotos/659038/monitor-gamer-aoc-24-fhd-ips-180hz-0-5ms-hdmi-e-dp-w-led-antireflexo-ajuste-de-altura-preto-24g4-p_1733834296_m.jpg"></v-img>
+                </div>
+                <v-card-text>
+                  <h4>Monitor Gamer AOC 24", Full HD, 180Hz, 0.5ms, IPS, DisplayPort e HDMI, Altura Ajustável, Preto -
+                    24G4/P
+                  </h4>
+                  <div class="my-4">
+                    <div class="font-weight-light text-decoration-line-through mb-n2">R$ 1.477,26</div>
+                    <div class="text-h5 font-weight-bold">R$ 749,99</div>
+                    <div class="font-weight-light">À vista no PIX</div>
+                  </div>
+                  <v-btn flat block color="primary">Adicionar</v-btn>
+                </v-card-text>
+              </v-card>
+            </v-col>
+
+            <v-col cols="12" sm="6" md="4" lg="3">
+              <v-card flat class="border">
+                <div class="px-6 py-2">
+                  <v-img
+                    src="https://images.kabum.com.br/produtos/fotos/659038/monitor-gamer-aoc-24-fhd-ips-180hz-0-5ms-hdmi-e-dp-w-led-antireflexo-ajuste-de-altura-preto-24g4-p_1733834296_m.jpg"></v-img>
+                </div>
+                <v-card-text>
+                  <h4>Monitor Gamer AOC 24", Full HD, 180Hz, 0.5ms, IPS, DisplayPort e HDMI, Altura Ajustável, Preto -
+                    24G4/P
+                  </h4>
+                  <div class="my-4">
+                    <div class="font-weight-light text-decoration-line-through mb-n2">R$ 1.477,26</div>
+                    <div class="text-h5 font-weight-bold">R$ 749,99</div>
+                    <div class="font-weight-light">À vista no PIX</div>
+                  </div>
+                  <v-btn flat block color="primary">Adicionar</v-btn>
+                </v-card-text>
+              </v-card>
+            </v-col>
+
+            <v-col cols="12" sm="6" md="4" lg="3">
+              <v-card flat class="border">
+                <div class="px-6 py-2">
+                  <v-img
+                    src="https://images.kabum.com.br/produtos/fotos/659038/monitor-gamer-aoc-24-fhd-ips-180hz-0-5ms-hdmi-e-dp-w-led-antireflexo-ajuste-de-altura-preto-24g4-p_1733834296_m.jpg"></v-img>
+                </div>
+                <v-card-text>
+                  <h4>Monitor Gamer AOC 24", Full HD, 180Hz, 0.5ms, IPS, DisplayPort e HDMI, Altura Ajustável, Preto -
+                    24G4/P
+                  </h4>
+                  <div class="my-4">
+                    <div class="font-weight-light text-decoration-line-through mb-n2">R$ 1.477,26</div>
+                    <div class="text-h5 font-weight-bold">R$ 749,99</div>
+                    <div class="font-weight-light">À vista no PIX</div>
+                  </div>
+                  <v-btn flat block color="primary">Adicionar</v-btn>
+                </v-card-text>
+              </v-card>
+            </v-col>
+
+            <v-col cols="12" sm="6" md="4" lg="3">
+              <v-card flat class="border">
+                <div class="px-6 py-2">
+                  <v-img
+                    src="https://images.kabum.com.br/produtos/fotos/659038/monitor-gamer-aoc-24-fhd-ips-180hz-0-5ms-hdmi-e-dp-w-led-antireflexo-ajuste-de-altura-preto-24g4-p_1733834296_m.jpg"></v-img>
+                </div>
+                <v-card-text>
+                  <h4>Monitor Gamer AOC 24", Full HD, 180Hz, 0.5ms, IPS, DisplayPort e HDMI, Altura Ajustável, Preto -
+                    24G4/P
+                  </h4>
+                  <div class="my-4">
+                    <div class="font-weight-light text-decoration-line-through mb-n2">R$ 1.477,26</div>
+                    <div class="text-h5 font-weight-bold">R$ 749,99</div>
+                    <div class="font-weight-light">À vista no PIX</div>
+                  </div>
+                  <v-btn flat block color="primary">Adicionar</v-btn>
+                </v-card-text>
+              </v-card>
+            </v-col>
+
+            <v-col cols="12" sm="6" md="4" lg="3">
+              <v-card flat class="border">
+                <div class="px-6 py-2">
+                  <v-img
+                    src="https://images.kabum.com.br/produtos/fotos/659038/monitor-gamer-aoc-24-fhd-ips-180hz-0-5ms-hdmi-e-dp-w-led-antireflexo-ajuste-de-altura-preto-24g4-p_1733834296_m.jpg"></v-img>
+                </div>
+                <v-card-text>
+                  <h4>Monitor Gamer AOC 24", Full HD, 180Hz, 0.5ms, IPS, DisplayPort e HDMI, Altura Ajustável, Preto -
+                    24G4/P
+                  </h4>
+                  <div class="my-4">
+                    <div class="font-weight-light text-decoration-line-through mb-n2">R$ 1.477,26</div>
+                    <div class="text-h5 font-weight-bold">R$ 749,99</div>
+                    <div class="font-weight-light">À vista no PIX</div>
+                  </div>
+                  <v-btn flat block color="primary">Adicionar</v-btn>
+                </v-card-text>
+              </v-card>
+            </v-col>
+
+          </v-row>
+
+        </v-container>
       </v-main>
 
       <AppFooter />
@@ -21,5 +266,5 @@
 </template>
 
 <script setup>
-  //
+//
 </script>
