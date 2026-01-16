be901955ff20d56e45ecb771f1af43be72ffb7c2
Brenno Luan <brennoluan8530@gmail.com>
2026-01-16 20:30:41 -0300

    refactor: restructure App.vue layout with app bar and improved theme

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
-  <v-app>
-    <v-main>
-      <HelloWorld />
-    </v-main>
+  <v-app theme="light">
+    <v-layout>
+      <v-app-bar flat density="comfortable" class="text-center border-b">
+        <v-app-bar-title>Vuetify Cart</v-app-bar-title>
 
-    <AppFooter />
+        <template #append>
+          <v-badge color="info" dot>
+            <v-icon icon="mdi-cart" />
+          </v-badge>
+        </template>
+      </v-app-bar>
+
+      <v-main>
+        <HelloWorld />
+      </v-main>
+
+      <AppFooter />
+    </v-layout>
   </v-app>
 </template>
 
